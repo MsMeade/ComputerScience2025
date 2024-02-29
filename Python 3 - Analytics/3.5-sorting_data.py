8be5455613f92c6226bc230d2eca251a32ef70ce
MsMeade <134280029+MsMeade@users.noreply.github.com>
2024-02-29 12:38:55 +0000

    Create 3.5-sorting_data.py

---
+++ Python 3 - Analytics/3.5-sorting_data.py
@@ -0,0 +1,10 @@
+myList=[3,7,1,88,6,50,4]
+print("original unsorted list ",myList)
+
+#Arrange list in assending order
+myList.sort()
+print("sorted list in ascending order ",myList)
+
+#Arrange list in decending order
+myList.sort(reverse=True)
+print("sorted list in decending orer ",myList)

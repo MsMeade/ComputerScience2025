23e27d0cb4d0e4120cc619abcdc4469403a20472
MsMeade <134280029+MsMeade@users.noreply.github.com>
2024-03-07 18:12:33 +0000

    Create 3.14-finding_the_mode2.py

---
+++ Python 3 - Analytics/3.14-finding_the_mode2.py
@@ -0,0 +1,10 @@
+import statistics
+
+# Initialise a list of values
+values = [2,3,5,2,4] 
+
+# Compute the mode
+modal_value = statistics.mode(values)
+
+# Display the answers
+print("The mode is ", modal_value)

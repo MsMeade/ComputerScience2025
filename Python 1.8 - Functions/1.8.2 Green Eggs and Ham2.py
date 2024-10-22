780d1f172f3c1214a678cb1de6264cb2ec7c59c1
MsMeade <134280029+MsMeade@users.noreply.github.com>
2024-10-22 20:58:25 +0100

    Create 1.8.2 Green Eggs and Ham2.py

---
+++ Python 1.8 - Functions/1.8.2 Green Eggs and Ham2.py
@@ -0,0 +1,18 @@
+# A program to display Green Eggs and Ham (v2)
+def displayChorus():
+    print()
+    print("I do not like green eggs and ham.")
+    print("I do not like them Sam-I-am.")
+    print()
+
+displayChorus()    
+print("I do not like them here or there.")
+print("I do not like them anywhere.")
+print("I do not like them in a house")
+print("I do not like them with  a mouse")
+displayChorus()
+print("I do not like them in a box")
+print("I do not like them with a fox")
+print("I will not eat them in the rain.")
+print("I will not eat them on a train")
+displayChorus()

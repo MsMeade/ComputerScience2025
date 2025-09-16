5f1635b1b4684157bda0923625e97c452fb887f7
MsMeade <134280029+MsMeade@users.noreply.github.com>
2025-09-16 21:55:58 +0100

    Add files via upload

---
+++ TY Module/TY Print.py
@@ -0,0 +1,7 @@
+print("Hello World \n My name is Ms.Meade")
+print("Hi everyone \t \t \t it is a nice day")
+
+
+print("*****")
+print("*   *")
+print("*****")
---
+++ TY Module/TY operators.py
@@ -0,0 +1,9 @@
+# These are numeric operators.
+print(30+5) #35
+print(30-5) #25
+print(30*5) #150
+print(30/5) #6
+print(2**5) #32
+print(8//7) #1
+print(8%7)  #1
+print(800%63)

572e737274c238c8be6a4cf3b4bda6a2b66db2ad
MsMeade <134280029+MsMeade@users.noreply.github.com>
2025-09-16 22:03:16 +0100

    Add files via upload

---
+++ 01. Python 1.1 - Getting Started/escSequence1.py
@@ -0,0 +1,1 @@
+print("In the words of Nelson Mandela, \"Education is the most powerful weapon which we can use to change the world\"")
---
+++ 01. Python 1.1 - Getting Started/escSequence2.py
@@ -0,0 +1,2 @@
+print("A\tB\nC") # A      B new line C
+print("C:\\Users\\johndoe\\Documents\\myfile.txt") # double backslash replace with a single backslash

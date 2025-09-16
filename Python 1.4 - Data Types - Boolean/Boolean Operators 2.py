c559fb8f4ffdb2bf048e168b93964a98d01786d5
MsMeade <134280029+MsMeade@users.noreply.github.com>
2025-09-16 10:08:17 +0100

    Add files via upload

---
+++ Python 1.4 - Data Types - Boolean/Boolean Operators 2.py
@@ -0,0 +1,16 @@
+print("computer"=="computer")
+print("computer"=="Computer")
+print("computer"!="Computer")
+
+print("67" is 67) #False
+print("Hello" is "Hello")
+print("r" in "rains")
+print("ia" in "rain")
+
+print("a" in "rain" and "i" in "rain")
+print("b" in "brain" or "z" in "brain")
+print(not("b" in "brain")) # not(True)
+print(not(True))
+print(not(False))
+
+print("1" in 12)
---
+++ Python 1.4 - Data Types - Boolean/Boolean Operators.py
@@ -0,0 +1,12 @@
+x=1
+y=0
+z=-1
+
+print(5>x) 		#True
+print(x>y)			#True
+print(x<=y)		#False
+print(y<=0)		#True
+print(z>y)			#False
+print(x==z)		#False
+print(0==y)		#True
+print(x!=y)		#True

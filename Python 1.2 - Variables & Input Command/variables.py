a89e6d2d3771fb16b1c359242ef1b98896f9107d
MsMeade <134280029+MsMeade@users.noreply.github.com>
2025-09-16 10:15:11 +0100

    Add files via upload

---
+++ Python 1.2 - Variables & Input Command/variables.py
@@ -0,0 +1,8 @@
+personName = "Alex"  # This variable name is written in camelCase
+favourite_colour = "Red" #This variabel is written in snake_case
+print("Hi", personName, " - Your favourite colour is", favourite_colour)
+print("Goodbye", personName)
+
+myAge = 17  
+favouriteHobby = "Dancing"
+print("Hello everyone, I am ", myAge, " years old and my favourite thing to do is", favouriteHobby)

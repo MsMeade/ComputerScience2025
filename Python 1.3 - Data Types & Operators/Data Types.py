140d891f284d7d7f4f795f604a01ede9fa9ac142
MsMeade <134280029+MsMeade@users.noreply.github.com>
2025-09-16 10:08:35 +0100

    Add files via upload

---
+++ Python 1.3 - Data Types & Operators/Data Types.py
@@ -0,0 +1,17 @@
+x = 5
+print(type(x))
+
+y = "Hello World"
+print(type(y))
+
+z = 8.7989898
+print(type(z))
+
+a = True
+print(type(a))
+
+b= ["kettle", "Iron", "Fridge"]
+print(type(b))
+
+c = ("this", "is", "a", "tuple")
+print(type(c))

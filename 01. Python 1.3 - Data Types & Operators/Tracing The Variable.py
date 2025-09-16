6332a3049ec615c9be9a7b1fa772435397cea7fe
MsMeade <134280029+MsMeade@users.noreply.github.com>
2025-09-16 22:00:46 +0100

    Add files via upload

---
+++ 01. Python 1.3 - Data Types & Operators/Tracing The Variable.py
@@ -0,0 +1,50 @@
+#Question 1
+# goals = 0
+# goals = goals + 1
+# print("The value of goals is", goals)
+
+#Question 2
+# answer = 1+2
+# print(answer)
+# value1 = answer+3
+# value2 = 1+2+3
+# print(value1, value2)
+
+#Question 3
+# a = 10
+# b = 5
+# temp = a
+# a = b
+# b = temp
+# print(a, b, temp)
+
+
+#Question 4
+# accountBalance = 1000
+# withdrawalAmount = 600
+# accountBalance = accountBalance - withdrawalAmount
+# print("accountBalance")
+
+#Question 5
+# days = 2
+# hrs = 24
+# mins = 60
+# total = days*hrs*mins
+# print(total)
+
+
+x = 8			# x=8 y= z=
+y = 3			# x=8 y=3 z=
+z = 9			# x=8 y=3 z=9
+x = z			# x=9 y=3 z=9
+y = x-z			# x=9 y=0 z=9
+z = "x"			# x=9 y=0 z=x
+x= 2**y			# x=1 y=0 z=x
+print(x,y,z)	# 1 0 x
+
+
+
+
+
+
+

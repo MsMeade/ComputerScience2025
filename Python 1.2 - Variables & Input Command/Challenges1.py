d997958127762f23b7c2b623c5177039dffdfba1
MsMeade <134280029+MsMeade@users.noreply.github.com>
2025-09-16 10:16:28 +0100

    Add files via upload

---
+++ Python 1.2 - Variables & Input Command/Challenges1.py
@@ -0,0 +1,12 @@
+#Challenge 1
+first_name = input("What is your first name?")
+print("Hello", first_name)
+
+
+#Challenge 2
+first_name = input("What is your first name?")
+surname = input("What is your surname?")
+print("Hello", first_name, surname)
+
+#Challenge 3
+print("What do you call a bear with no teeth? \n A gummy bear")

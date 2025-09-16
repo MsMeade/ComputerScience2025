4add767a02d86332a33986f0879360496b86d781
MsMeade <134280029+MsMeade@users.noreply.github.com>
2025-09-16 10:09:23 +0100

    Add files via upload

---
+++ Python 1.3 - Data Types & Operators/fahrenheit to celsius.py
@@ -0,0 +1,7 @@
+# centigrade = float(input("Enter your centigrade value: "))
+# fahrenheit = (centigrade*(9/5))+32
+# print(centigrade,"degrees celsius, is equal to",fahrenheit,"degrees fahrenheit")
+
+f = float(input("Enter your centigrade value: "))
+c = (f-32)*(5/9)
+print(f,"degrees fahrenheit, is equal to",c,"degrees celsius")

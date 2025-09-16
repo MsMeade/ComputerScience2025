d0d66d544aacb687c7750b95b1331888a4408814
MsMeade <134280029+MsMeade@users.noreply.github.com>
2025-09-16 10:05:03 +0100

    Add files via upload

---
+++ Python 1.2 - Variables & Input Command/Input1.py
@@ -0,0 +1,4 @@
+personName = input("Enter your name: ") 
+favourite_colour = input("Enter your favourite colour: ") 
+print("Hi", personName, " - Your favourite colour is", favourite_colour)
+print("Goodbye", personName)
---
+++ Python 1.2 - Variables & Input Command/Input2.py
@@ -0,0 +1,6 @@
+user_birthday = input("Enter your birthday: ") 
+favourite_colour = input("Enter your favourite colour: ")
+favourite_clothing= input("Enter your favourite piece of clothing: ")
+print("I know what would be perfect for your birthday on the", user_birthday, " . It would be a", favourite_colour, favourite_clothing)
+
+print(f"I know what would be the perfect gift for your birthday on the {birthday}. It would be a {colour} {clothing}!")
